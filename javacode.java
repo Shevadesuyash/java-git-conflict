9c87c0d4a01b483f49301b5570f9b7b84337bfd5
Shevadesuyash <shevadesuyash@gmail.com>
2024-07-06 16:01:47 +0530

    initial commit

---
+++ javacode.java
@@ -0,0 +1,14 @@
+public class javacode {
+
+    public static void main(String[] args) {
+        System.out.println("Hi My name is suyash ");
+
+    }
+    public static int add (int a,int b) {
+        int add=a+b;
+        System.out.println("addition");
+
+    }
+
+
+}
