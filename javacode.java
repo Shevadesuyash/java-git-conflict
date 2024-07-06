6be257dca3547c72b200aac243f3c9816a782e92
Shevadesuyash <shevadesuyash@gmail.com>
2024-07-06 16:44:13 +0530

    commit in main  update error

--- javacode.java
+++ javacode.java
@@ -4,6 +4,7 @@ public class javacode {
         System.out.println("Hi My name is suyash ");
         int x=add(2,4);
         int y= op(10,5);
+        int q= add1(8);
 
     }
     public static int add (int a,int b) {
@@ -17,6 +18,12 @@ public class javacode {
         int x=a-b;
         return x;
     }
+    public static int add1(int a) {
+        return a+2;
+    }
+
+
+
 
 
 }
